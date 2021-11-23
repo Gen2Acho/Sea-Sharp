3b89dc658b503041cb15b59960e333a40d8f8fd7
Gen2Acho <92388332+Gen2Acho@users.noreply.github.com>
2021-11-23 16:40:25 +0200

    Add files via upload

---
+++ 221 Q1.cs
@@ -0,0 +1,25 @@
+
+using System;
+
+namespace methodz
+{
+    class Program
+    {
+        static void PrintString()
+        {
+            Console.WriteLine ("This method has the 'return type void' and 'No parameters'  ");
+        }
+
+        // The Main method for the program
+        static void Main(string[] args)
+        {
+
+            /*Calling the method*/
+            PrintString();
+
+            Console.ReadLine(); // What's the purpose of this line?
+            //The purpose is to wait for the user to enter any key to exit the program 		---TRY TO CHANGE UP THE ENGLISH, KANA REA KGWETSA DAH!!---
+        }   //end Main
+
+    }
+}
