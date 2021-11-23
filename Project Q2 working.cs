e21148f4d9df36f04ae463a5c2761cf07accaf6f
Gen2Acho <92388332+Gen2Acho@users.noreply.github.com>
2021-11-23 16:42:44 +0200

    Add files via upload

---
+++ Project Q2 working.cs
@@ -0,0 +1,52 @@
+﻿/*using System;
+using System.Collections.Generic;
+
+namespace Project_draft
+{
+    public class ECheck
+    {
+        private static List<string> _structures = new List<string>();
+
+        public static void Shift(List<string> @struct)
+        {
+            List<string> checkStruct = new List<string>();
+            _structures = @struct;
+            checkStruct.Add(_structures[0]);
+            for (int i = 0; i < _structures.Count; i++)
+            {
+                for (int x = 0; x < _structures[i].Length; x++)
+                {
+                    string temp = _structures[i].Substring(x);
+                    string a = Convert.ToString(_structures[i][x]);
+                    string b = temp.Insert(temp.Length, a);
+                    checkStruct.Add(b);
+                }
+            }
+
+            if (checkStruct.Contains(_structures[1]))
+            {
+                Console.WriteLine("Structures are equivalent.");
+            }
+            else
+            {
+                Console.WriteLine("Structures are Not equivalent.");
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> structures = new List<string>();
+            for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine("Enter structure {0}", (i+1));
+                    structures.Add(Console.ReadLine());
+                }
+
+            ECheck.Shift(structures);
+        }
+    }
+}*/
---
+++ Shift.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+namespace COMP201_PROJECT
+{
+    public class Shift
+    {
+        private List<string> Structures = new Shift();e
+    }
+}
